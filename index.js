ec088b2e4b0d710eb00efbfc57cb7b25d72b3bce
Kaitlyn Peeples <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-02-11 21:54:34 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to my static site!</h1>
+      <p>This is a paragraph I added to the site.</p>
+    </div>
+  );
+};
+export default IndexPage;
